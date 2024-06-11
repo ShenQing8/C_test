28f585cbf0daf10e45b844e9e0f6f0e33557333c
ShenQing8 <146062753+ShenQing8@users.noreply.github.com>
2024-06-11 17:57:32 +0800

    test

---
+++ choose/Project1/choose.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int 要 = 0;
+	int 不要 = 1;
+	int input = 0;
+	printf("要抽芙芙吗？");
+	scanf("%d", &input);
+	if (input == 0)
+	{
+		printf("欢迎加入芙门！\n");
+	}
+	else
+	{
+		printf("死刑！必须死刑！！！\n");
+	}
+	return 0;
+}
