51a21ca340afde1352382ac00560bfe2cc7a3650
SQ <1653216274@qq.com>
2024-06-22 17:03:47 +0800

    .

--- Dynamic memory management/Dynamic memory management/test.c
+++ Dynamic memory management/Dynamic memory management/test.c
@@ -69,39 +69,49 @@
 
 //realloc
 
+//int main()
+//{
+//	//开辟
+//	int* p = (int*)malloc(40);
+//	if (p == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 1;
+//	}
+//	//使用
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i + 1;
+//	}
+//	//扩容
+//	int* ptr = (int*)realloc(p, 8000);
+//	//realloc开辟的空间不可直接赋给p
+//	//可能开辟空间失败，则返回空指针，便将p改为了NULL
+//
+//	//realloc扩容空间时有两种情况
+//	//1、原空间后面的空间够大，则直接扩容
+//	//2、若原空间后面的空间被使用了，不够大，则在内存中重新找一块够大的空间
+//	//并将数据拷贝过来，将原空间释放
+//	if (ptr != NULL)
+//	{
+//		p = ptr;
+//		ptr = NULL;
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
 int main()
 {
-	//开辟
 	int* p = (int*)malloc(40);
-	if (p == NULL)
+	while (1)
 	{
-		printf("%s\n", strerror(errno));
-		return 1;
-	}
-	//使用
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		*(p + i) = i + 1;
-	}
-	//扩容
-	int* ptr = (int*)realloc(p, 8000);
-	//realloc开辟的空间不可直接赋给p
-	//可能开辟空间失败，则返回空指针，便将p改为了NULL
-
-	//realloc扩容空间时有两种情况
-	//1、原空间后面的空间够大，则直接扩容
-	//2、若原空间后面的空间被使用了，不够大，则在内存中重新找一块够大的空间
-	//并将数据拷贝过来，将原空间释放
-	if (ptr != NULL)
-	{
-		p = ptr;
-		ptr = NULL;
-	}
-
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", *(p + i));
+		p = realloc(p, 40);
 	}
 	return 0;
 }
